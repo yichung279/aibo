f647d6b0587a9d4d5db2d43adf32a3df0cd82ce7
low0621 <low0621@gmail.com>
2020-07-14 13:35:19 +0800

    Add callback route

--- main.py
+++ main.py
@@ -1,15 +1,60 @@
 #!/usr/bin/env python3
 
-import  uvicorn
-from fastapi import FastAPI
+
+# third-party imports
+import uvicorn
+from fastapi import FastAPI, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from pydantic import BaseModel
+
+# local imports
 import config
 
 app = FastAPI()
+line_bot_api = LineBotApi(config.access_token) # Channel Access Token
+handler = WebhookHandler(config.secret) # Channel Secret
 
+class Item(BaseModel):
+    destination: str
+    events: list
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post('/callback')
+async def callback(item: Item, request: Request):
+    signature = request.headers['X-Line-Signature'] # get X-Line-Signature header value
+    body = str(item) # get request body as text
+    # app.logger.info("Request body: " + body) # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError as e:
+        print(e)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    if event.message.text == '機器人你好':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text='Hi I\'m SinoPac AI bot')
+        )
+    if 'group' == event.source.type:
+        user_id = event.source.group_id
+    elif 'room' == event.source.type:
+        user_id = event.source.room_id
+    else:
+        user_id = event.source.user_id
+
+    conn = sqlite3.connect('crawler.db')
+    c = conn.cursor()
+    c.execute("INSERT OR IGNORE INTO user (userid) VALUES(\'%s\')" %user_id)
+    conn.commit()
+    print('user saved')
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=config.host, port=config.port, ssl_keyfile=config.key, ssl_certfile=config.cert)
